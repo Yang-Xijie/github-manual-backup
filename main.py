ee934dda1b888a50b377169dd3ee64b2ee67e544
yxj <564197835@qq.com>
2022-10-28 22:49:41 +0800

    add excluded repos

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import json
 import os
 import subprocess
 
+excluded_repos = ["eesast/courses", "Edgenesis/official_website"]
+
 with open("./output/repos.json") as f:
     repos = json.loads(f.read())
 print(f"{len(repos)} repositories found")
@@ -11,8 +13,12 @@ backup_folder = f'backup-{datetime.now().strftime("%Y%m%d-%H%M%S")}'
 for repo in repos:
     repo_ssh_url = repo["ssh_url"]
     repo_full_name = repo["full_name"]
-    print(f"start with repository {repo_full_name}")
+    if repo_full_name not in excluded_repos:
+        print(f"start with repository {repo_full_name}")
 
-    repo_folder = f"./{backup_folder}/{repo_full_name}"
-    os.makedirs(repo_folder)
-    subprocess.run([f"""git clone {repo_ssh_url} {repo_folder}"""], shell=True)
+        repo_folder = f"./{backup_folder}/{repo_full_name}"
+        os.makedirs(repo_folder)
+        subprocess.run([f"""git clone {repo_ssh_url} {repo_folder}"""], shell=True)
+        print(f"succeed")
+    else:
+        print(f"skip {repo_full_name}")
